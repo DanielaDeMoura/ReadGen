881298c2ae0b03f623d549665f40f6d00036585e
MissMoneyBags <106129216+DanielaDeMoura@users.noreply.github.com>
2024-01-22 03:05:02 +0000

    finished most off the index.js , I will finish the rest tomorrow.

--- index.js
+++ index.js
@@ -5,16 +5,41 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
-];
-
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "What does your project do? Describe your project.",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Instructions on how to use your project?",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "How do I install your project?",
+    },
+  ];
+  
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+    console.log(`Successfully generated ${fileName}`);
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then((answers) => {
+        const markdown = generateMarkdown(answers);
+        const fileName = "ReadMe.md";
+        writeToFile(fileName, markdown);
+    });
 }
 
 // function call to initialize program
